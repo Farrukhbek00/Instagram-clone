b12486441589743b2bda9eca95bff9bfa4325577
Farrukhbek00 <farruhzokirov00@gmail.com>
2020-09-24 11:13:09 +0500

    profile logic

--- freeCodeGram/app/Http/Controllers/ProfilesController.php
+++ freeCodeGram/app/Http/Controllers/ProfilesController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ProfilesController extends Controller
 {
-    public function index()
+    public function index($user)
     {
-        return view('home');
+        $user = User::findOrFail($user);
+        return view('home', [
+            'user' => $user,
+        ]);
     }
 }
--- freeCodeGram/resources/views/home.blade.php
+++ freeCodeGram/resources/views/home.blade.php
@@ -7,15 +7,15 @@
             <img src="https://instagram.ftas2-2.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.ftas2-2.fna.fbcdn.net&_nc_ohc=X9KyPasyhJkAX8D_cGd&oh=f45993727dee162766f65b1dc54ec4a1&oe=5F95E867" class="rounded-circle">
         </div>
         <div class="col-9 p-5">
-            <div><h1>{{ Auth::user()->username }}</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
-            <div><a href="#">www.freecodecamp.org</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title  }}</div>
+            <div>{{ $user->profile->description  }}</div>
+            <div><a href="#">{{ $user->profile->url}}</a></div>
         </div>
     </div>
 
