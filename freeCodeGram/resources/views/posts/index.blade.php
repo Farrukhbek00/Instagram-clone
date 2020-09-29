58456bf75dcebddea33754d3ddbaa3fb3d4d5694
Farrukhbek00 <farruhzokirov00@gmail.com>
2020-09-29 22:52:49 +0500

    caching and sending mail

--- freeCodeGram/resources/views/posts/index.blade.php
+++ freeCodeGram/resources/views/posts/index.blade.php
@@ -4,36 +4,29 @@
     <div class="container">
         @foreach($posts as $post)
             <div class="row">
-                <div class="col-8">
-                    <img src="/storage/{{ $post->image  }}" class="w-100">
+                <div class="col-6 offset-3">
+                    <a href="/profile/{{ $post->user->id }}"><img src="/storage/{{ $post->image  }}" class="w-100"></a>
                 </div>
-                <div class="col-4">
+            </div>
+            <div class="row pt-2 pb-4">
+                <div class="col-6 offset-3">
                     <div>
-                        <div class="d-flex align-items-center">
-                            <div class="pr-3">
-                                <img src="{{ $post->user->profile->profileImage() }}" class="w-100 rounded-circle" style="max-width: 40px;">
-                            </div>
-                            <div>
-                                <div class="font-weight-bold">
-                                    <a href="/profile/{{ $post->user->id }}">
-                                        <span class="text-dark">{{ $post->user->username }}</span>
-                                    </a>  |
-                                    <a href="#" class="pl-3">Follow</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <hr>
+                        <p>
+                            <span class="font-weight-bold">
+                                <a href="/profile/{{ $post->user->id }}">
+                                    <span class="text-dark">{{ $post->user->username }}</span>
+                                </a>
+                            </span> {{ $post->caption }}
+                        </p>
                     </div>
-
-                    <p><span class="font-weight-bold">
-                        <a href="/profile/{{ $post->user->id }}">
-                            <span class="text-dark">{{ $post->user->username }}</span>
-                        </a>
-                    </span> {{ $post->caption }}
-                    </p>
                 </div>
             </div>
         @endforeach
+
+            <div class="row">
+                <div class="col-12 justify-content-center">
+                    {{ $posts->links() }}
+                </div>
+            </div>
     </div>
 @endsection
